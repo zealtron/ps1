637c426243c1e9a7c208975d8bb53550a55f72e7
UVa student <student@student-VirtualBox.(none)>
2014-01-26 19:47:26 -0500

    DONE

--- zhttpto.rs
+++ zhttpto.rs
@@ -15,6 +15,8 @@
 use std::io::*;
 use std::io::net::ip::{SocketAddr};
 use std::{str};
+use std::io::buffered::BufferedReader;
+use std::io::File;
 
 static IP: &'static str = "127.0.0.1";
 static PORT:        int = 4414;
@@ -51,11 +53,12 @@ fn main() {
             println(format!("Received request :\n{:s}", request_str));
 	    let r: ~[&str] = request_str.split(' ').collect();
 	    if (r[0].contains("GET") && r[2].contains("HTTP/1.1")) {
-		request(r[1]);
+		let response: ~str = request(r[1]);
+		stream.write(response.as_bytes());
 
 	    }
 	    else {
-		print("TEST");
+		//print("TEST");
  		let response: ~str = 
                     ~"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
                      <doctype !html><html><head><title>Hello, Rust!</title>
@@ -88,7 +91,61 @@ fn main() {
     }
 }
 
-fn request(a: &str) {
-    print!("{:s}", a);
+fn request(a: &str) -> ~str{
+    //print!("{:s}", a);
+    let path = Path::new("/home/student/ps1" + a);
+    if (path.is_file() && a.contains(".html")){
+	let file = File::open(&path);
+	let mut reader = BufferedReader::new(file);
+	let contents = reader.read_to_str();
+	let response: ~str = 
+                    ~"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                     <doctype !html><html><head><title>Hello, Rust!</title>
+                     <style><!--
+				body {font-family: arial,sans-serif; text-align: center}
+				img { border:none; }
+				//-->
+               	     </style></head>
+                     <body>
+                     <h2>" + contents + "</h2>
+                     </body></html>\r\n";
+	return response;
+    } 
+    else if path.is_file() {
+	let response: ~str = 
+                    ~"HTTP/1.1 403 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                     <doctype !html><html><head><title>Error 403 Forbidden</title>
+                     <style><!--
+				body {font-family: arial,sans-serif}
+				img { border:none; }
+				//-->
+               	     </style></head>
+                     <body>
+		     <blockquote>
+		     <h2>Error 403 Forbidden</h2>
+		     <p>You don't have permission to acces this on this server.
+		     </blockquote>
+     		     </body></html>\r\n";
+	return response;
+	
+    }
+    else {
+        //println("NO FILE EXISTS");
+	let response: ~str = 
+                    ~"HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                     <doctype !html><html><head><title>Error 404 Not Found</title>
+                     <style><!--
+				body {font-family: arial,sans-serif}
+				img { border:none; }
+				//-->
+               	     </style></head>
+                     <body>
+		     <blockquote>
+		     <h2>Error 404 NOT FOUND</h2>
+		     <p>Our apologies for the temporary inconvenience. The requested URL was not found on this server.
+		     </blockquote>
+     		     </body></html>\r\n";
+	return response;
+    }
 
 }
